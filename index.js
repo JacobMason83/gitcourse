dafe996808c4f24fc6876cf3483e2463ca92f6f1
JacobMason83 <jmason062083@gmail.com>
2020-12-14 17:20:12 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// git init starts the git repository 
+// make sure you in the root directory where you want the repository to live 
+/*
+git status = tells you if you have files to be committed, branch your on, pretty much the status your at
+git add "the file"/and or (. = all the files )
+git commit -m "initial commit" = that your committing the file to master file, and your giving it the message initial commit / and creating the version of it
+
+*/
