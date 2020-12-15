0a1341894864602f1973219558e3f91290b6c1dc
JacobMason83 <jmason062083@gmail.com>
2020-12-15 11:27:48 -0500

    added readme file to repo

--- index.js
+++ index.js
@@ -4,5 +4,12 @@
 git status = tells you if you have files to be committed, branch your on, pretty much the status your at
 git add "the file"/and or (. = all the files )
 git commit -m "initial commit" = that your committing the file to master file, and your giving it the message initial commit / and creating the version of it
+git remote add origin https://github.com/JacobMason83/gitcourse.git https is whatever the repo name 
+use http version, and git remote -v 
+and it will give you a fetch, and push remotes and as long as you have that your good to go 
+or if you name it origin main, you can change it git branch master then hit enter
+then git push -U origin master 
+to make your repo, go to github, hit the plus sign, and name the repo as long as its green , and after that you click create repository
+quick setup allows you to quickly link your local system, and online repo
 
 */
